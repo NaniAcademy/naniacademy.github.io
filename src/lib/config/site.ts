907d17020e12162bd87ad3f4325431ead65f53df
Durga Prasad Reddy Vennapusa <durgaprasad.vennapusa@gmail.com>
2024-03-02 11:34:38 +0530

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,16 +2,16 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'naniacademy.github.io',
+  title: 'NaniAcademy',
+  subtitle: 'If Apps Can Speak',
   lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  description: 'This blog is all about explaining programming concepts by narrating the code written.',
   author: {
     avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
+    name: 'Durga Prasad',
     status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    bio: 'Passionate Software Engineer'
   },
   themeColor: '#3D4451'
 }
