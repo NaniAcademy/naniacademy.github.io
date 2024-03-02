e1970d7fb3832721d255e6e71093603998064303
Durga Prasad Reddy Vennapusa <durgaprasad.vennapusa@gmail.com>
2024-03-02 12:14:30 +0530

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'naniacademy.github.io',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'naniacademy.github.io/blog-site',
   title: 'NaniAcademy',
   subtitle: 'If Apps Can Speak',
   lang: 'en-US',
